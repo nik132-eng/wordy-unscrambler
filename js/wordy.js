c3cc942e98eb40b1632577cbe2fa65723b7abc4e
nikunj <77881638+nik132-eng@users.noreply.github.com>
2023-09-29 19:03:23 +0530

    update with trie algo

--- js/wordy.js
+++ js/wordy.js
@@ -3,65 +3,67 @@ export default {
 	findWords
 };
 
-var dict = [];
+
+var dict = {};
+var isWord = Symbol("is-word");
 
 function loadWords(wordList) {
-	// here our dictionary is just a (sorted) array of strings,
-	// nothing more fancy than that
-	dict = [ ...wordList ];
+	var nodeCount = 0;
 
-	return dict.length;
+	// reset a previously loaded dictionary?
+	if (Object.keys(dict).length > 0) {
+		dict = {};
+	}
+
+	// construct the dictionary as a trie
+	for (let word of wordList) {
+		// traverse down the trie (from the root), creating nodes
+		// as necessary
+		let node = dict;
+		for (let letter of word) {
+			if (!node[letter]) {
+				node[letter] = {
+					[isWord]: false,
+				};
+				nodeCount++;
+			}
+			node = node[letter];
+		}
+
+		// mark the terminal node for this word
+		node[isWord] = true;
+	}
+
+	return nodeCount;
 }
 
-function findWords(input) {
+function findWords(input,prefix = "",node = dict) {
 	var words = [];
 
-	// consult every word in our dictionary
-	for (let word of dict) {
-		if (
-			// enough input characters to construct the word?
-			input.length >= word.length &&
+	// is the current node the end of a valid word?
+	if (node[isWord]) {
+		words.push(prefix);
+	}
 
-			// did any permutations of the input match?
-			checkWord(word,input)
-		) {
-			words.push(word);
+	for (let i = 0; i < input.length; i++) {
+		let currentLetter = input[i];
+
+		// does the current (sub)trie have a node for this letter?
+		if (node[currentLetter]) {
+			let remainingLetters = [
+				...input.slice(0,i),
+				...input.slice(i + 1)
+			];
+			words.push(
+				...findWords(
+					remainingLetters,
+					prefix + currentLetter,
+					node[currentLetter]
+				)
+			);
 		}
 	}
 
+	words = [ ...(new Set(words)) ];
 	return words;
-}
-
-function checkWord(word,input) {
-	return permute("",input);
-
-	// permute the input letters (k! variations)
-	function permute(prefix,remaining) {
-		for (let i = 0; i < remaining.length; i++) {
-			let current = prefix + remaining[i];
-
-			// found a permutation that matched?
-			if (current == word) {
-				return true;
-			}
-			else if (
-				// any remaining input letters to permute and concat?
-				remaining.length > 1 &&
-
-				// current string shorter than the target word?
-				current.length < word.length
-			) {
-				// check all the permuted remaining letters
-				if (
-					permute(current,remaining.slice(0,i) + remaining.slice(i + 1))
-				) {
-					// propagate the match-found signal back up the
-					// recursion chain
-					return true;
-				}
-			}
-		}
-
-		return false;
-	}
 }
